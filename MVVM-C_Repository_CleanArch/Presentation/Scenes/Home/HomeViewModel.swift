6d12bc46ba9659e46e73114b02ca21b433a405c2
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-31 01:08:05 +0300

    HomeViewModel
    DI, Pop,

--- MVVM-C_Repository_CleanArch/Presentation/Scenes/Home/HomeViewModel.swift
+++ MVVM-C_Repository_CleanArch/Presentation/Scenes/Home/HomeViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by rico on 30.12.2025.
 //
 
+import Foundation
 
+@MainActor
+final class HomeViewModel: HomeViewModelProtocol {
+    weak var delegate: HomeViewModelDelegate?
+    private let repository: CharacterRepositoryProtocol
+    private(set) var characters: [Character] = []
+    
+    init(repository: CharacterRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func viewDidLoad() {
+        fetchCharacters()
+    }
+    
+    private func fetchCharacters() {
+        delegate?.didChangeState(state: .loading)
+        
+        Task {
+            let result = await repository.fetchCharacters()
+            switch result {
+            case .success(let charactersData):
+                self.characters = charactersData
+                delegate?.didChangeState(state: .success)
+                
+            case .failure(let error):
+                delegate?.didChangeState(state: .failure(error.localizedDescription))
+            }
+        }
+    }
+}
